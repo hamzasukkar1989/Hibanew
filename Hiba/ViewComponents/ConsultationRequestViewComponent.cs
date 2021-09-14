926f8a582ab3e35d87c356557553ecba65c507c7
hamzasukkar1989 <hamzasukkar1989@outlook.com>
2021-09-14 11:41:52 +0300

    remove contact from HomeTuseday

--- Hiba/ViewComponents/ConsultationRequestViewComponent.cs
+++ Hiba/ViewComponents/ConsultationRequestViewComponent.cs
@@ -5,12 +5,15 @@ using System.Linq;
 using System.Threading.Tasks;
 using Hiba.Services.Contracts;
 using System.Threading;
+using System.Globalization;
 
 namespace Hiba.ViewComponents
 {
     public class ConsultationRequestViewComponent : ViewComponent
     {
         private readonly IConsultationRequest _consultationRequest;
+        CultureInfo uiCultureInfo = Thread.CurrentThread.CurrentUICulture;
+        CultureInfo cultureInfo = Thread.CurrentThread.CurrentCulture;
         public ConsultationRequestViewComponent(IConsultationRequest consultationRequest)
         {
             _consultationRequest = consultationRequest;
@@ -19,6 +22,11 @@ namespace Hiba.ViewComponents
         public async Task<IViewComponentResult> InvokeAsync()
         {
             var _consultationrequest = await _consultationRequest.ConsultationRequest();
+            ViewBag.ConsultationRequest = "Consultation Request";
+            if (cultureInfo.ToString() == "ar")
+            {
+                ViewBag.ConsultationRequest = "طلب أستشارة";
+            }          
             return View(_consultationrequest);
         }
     }
