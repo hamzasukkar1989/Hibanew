7349002a23ede4c44486661a475c095a81ed9e69
hamzasukkar1989 <hamzasukkar1989@outlook.com>
2021-09-19 16:56:47 +0300

    Add Contact Model

---
+++ Hiba/Models/Contact.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hiba.Models
+{
+    public class Contact
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int Phone { get; set; }
+        public int Age { get; set; }
+        public string Country { get; set; }
+        public string Education { get; set; }
+        public string Work { get; set; }
+        public string Message { get; set; }
+    }
+}
