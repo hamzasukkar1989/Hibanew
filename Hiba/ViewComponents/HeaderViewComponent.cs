ca8d7484e34c3e0d4a443f0693f2f4ed436facca
hamzasukkar1989 <hamzasukkar1989@outlook.com>
2021-09-30 16:45:11 +0300

    Add ViewBag.RegisterNow To Banner

--- Hiba/ViewComponents/HeaderViewComponent.cs
+++ Hiba/ViewComponents/HeaderViewComponent.cs
@@ -4,12 +4,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Hiba.Services.Contracts;
+using System.Globalization;
+using System.Threading;
 
 namespace Hiba.ViewComponents
 {
     public class HeaderViewComponent :ViewComponent
     {
         private readonly IHeader _header;
+        CultureInfo uiCultureInfo = Thread.CurrentThread.CurrentUICulture;
+        CultureInfo cultureInfo = Thread.CurrentThread.CurrentCulture;
 
         public HeaderViewComponent(IHeader header)
         {
@@ -17,6 +21,12 @@ namespace Hiba.ViewComponents
         }
         public async Task<IViewComponentResult> InvokeAsync()
         {
+
+            ViewBag.RegisterNow = "left";
+            if (cultureInfo.ToString() == "ar")
+            {
+                ViewBag.RegisterNow = "right";
+            }
             var Baners = await _header.GetBanners();
             return View(Baners);
         }
